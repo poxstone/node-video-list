e2b5967e888f60472cb9e331584f6252ee5d0c5b
Lozadamiguel0512 <Lozadamiguel0512@gmail.com>
2018-01-19 17:05:41 -0500

    add design structure

--- db.sh
+++ db.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name mysql -p 3306:3306 -v /home/lozadamiguel0512/mysqldb/:/etc/mysql/conf.d -e MYSQL_DATABASE=videosdb -e MYSQL_ROOT_PASSWORD=videopass -e MYSQL_USER=vudeouser -e MYSQL_PASSWORD=videopass -d mysql:5.7
+docker run --name mysql -p 3306:3306 -v $HOME/mysqldb/:/var/lib/mysql -e MYSQL_DATABASE=videosdb -e MYSQL_ROOT_PASSWORD=videopass -e MYSQL_USER=vudeouser -e MYSQL_PASSWORD=videopass -d mysql:5.7
